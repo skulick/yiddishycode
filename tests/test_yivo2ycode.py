1b72b3d707f3ec1a8d3695ae03369781b798056a
Seth Kulick <skulick@ldc.upenn.edu>
2021-10-28 21:04:39 -0400

    changes

---
+++ tests/test_yivo2ycode.py
@@ -0,0 +1,13 @@
+import unicodedata
+from yiddishycode.translit import Transliterator
+
+def main():
+    transliterator = Transliterator()
+    x = 1
+    for yivo_text in [
+            'vos', 'shoyn', 'ingantsn', 'azoy']:
+        ycode_text = transliterator.yivo2ycode(yivo_text)
+        print(f'{yivo_text} -> {ycode_text}')
+
+if __name__ == '__main__':
+    main()
--- yiddishycode/translit.py
+++ yiddishycode/translit.py
@@ -46,6 +46,24 @@ def read_trans_table():
 
     return (ycode_chars, yiddish_chars)
 
+def read_yivo2ycode_table():
+    with pkg_resources.open_text(tables, 'yivo-ycode.txt') as fin:
+        lines = fin.readlines()
+
+    lines = [line.rstrip() for line in lines]
+    lines = [line.split('\t') for line in lines
+             if line and not line.startswith(';;')]
+
+    yivo_chars = [line[0].strip() for line in lines]
+    ycode_chars = [line[1].strip() for line in lines]
+    yivo2ycode_1 = {yivo_char: ycode_char
+                    for (yivo_char, ycode_char) in zip(yivo_chars, ycode_chars)
+                    if len(yivo_char) == 1}
+    yivo2ycode_2 = {yivo_char: ycode_char
+                  for (yivo_char, ycode_char) in zip(yivo_chars, ycode_chars)
+                  if len(yivo_char) == 2}
+    return (yivo2ycode_1, yivo2ycode_2)
+
 
 class Transliterator:
     def __init__(self):
@@ -56,6 +74,8 @@ class Transliterator:
             yiddish_str, ycode_str)
         self.trans_ycode2yiddish = str.maketrans(
             ycode_str, yiddish_str)
+        (self.yivo2ycode_1, self.yivo2ycode_2) = read_yivo2ycode_table()
+
 
     def yiddish2ycode(self, yiddish_text):
         """Convert Yiddish unicode to ascii ycode"""
@@ -72,3 +92,37 @@ class Transliterator:
         yiddish_text = ycode_text.translate(
             self.trans_ycode2yiddish)
         return yiddish_text
+
+    def yivo2ycode(self, yivo_text):
+        ycode_text = ''
+        yivo_x = 0
+        while yivo_x < len(yivo_text) - 1:
+            chr1 = yivo_text[yivo_x]
+            chr2 = yivo_text[yivo_x+1]
+            chr12 = chr1 + chr2
+            if chr12 in self.yivo2ycode_2:
+                ycode_text += self.yivo2ycode_2[chr12]
+                yivo_x += 2
+            elif chr1 in self.yivo2ycode_1:
+                ycode_text += self.yivo2ycode_1[chr1]
+                yivo_x += 1
+            else:
+                print(f'unknown character {chr1}')
+                ycode_text += chr1
+                yivo_x += 1
+                
+        if yivo_x < len(yivo_text):
+            chr1 = yivo_text[yivo_x]
+            if chr1 in self.yivo2ycode_1:
+                ycode_text += self.yivo2ycode_1[chr1]
+                yivo_x += 1
+            else:
+                print(f'unknown character {chr1} {yivo_x}')
+                ycode_text += chr1
+                yivo_x += 1
+                
+        chr1 = ycode_text[-1]
+        if chr1 in ('xmnfq'):
+            chr1u = chr1.upper()
+            ycode_text = ycode_text[:-1] + chr1u
+        return ycode_text
