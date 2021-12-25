f5d3041208598ad27d6710408b6d2092cb1eeb4e
Seth Kulick <skulick@ldc.upenn.edu>
2021-12-25 13:48:21 -0500

    changes

--- tests/test_yivo2ycode.py
+++ tests/test_yivo2ycode.py
@@ -1,14 +1,14 @@
 import unicodedata
-from yiddishycode.translit import Transliterator
+from yiddishycode.translit_yivo import TransliteratorYivo
 
 def main():
-    transliterator = Transliterator()
+    translit_yivo = TransliteratorYivo()
     x = 1
     for yivo_text in [
             'vos', 'shoyn', 'ingantsn', 'azoy', 'kontshn', 'yerushe',
             'oysgekratst', 'yerushe', 'nadn', 'avrom-yankev',
             'vuhin']:
-        ycode_text = transliterator.yivo2ycode(yivo_text)
+        ycode_text = translit_yivo.yivo2ycode(yivo_text)
         print(f'{yivo_text} -> {ycode_text}')
 
 if __name__ == '__main__':
